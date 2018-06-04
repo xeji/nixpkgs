1a63413b7ed3d183367062c6554a9ece62ef410a
Uli Baum <xeji@cat3.de>
2018-06-04 12:19:08 +0200

    [WIP] blivet: 0.67 -> 3.0.1

--- pkgs/development/python-modules/blivet/default.nix
+++ pkgs/development/python-modules/blivet/default.nix
@@ -1,47 +1,63 @@
-{ stdenv, fetchFromGitHub, buildPythonPackage, pykickstart, pyparted, pyblock
-, pyudev, six, libselinux, cryptsetup, multipath-tools, lsof, utillinux
+{ stdenv, fetchFromGitHub, pythonPackages 
+, libselinux, cryptsetup
+, lsof
+, multipath-tools
+, utillinux
+, enum
 }:
 
 let
-  pyenable = { enablePython = true; };
-  selinuxWithPython = libselinux.override pyenable;
-  cryptsetupWithPython = cryptsetup.override pyenable;
-in buildPythonPackage rec {
+  cryptsetupWithPython = cryptsetup.override { enablePython = true; };
+in pythonPackages.buildPythonPackage rec {
   pname = "blivet";
   name = "${pname}-${version}";
-  version = "0.67";
+  version = "3.0.1";
 
   src = fetchFromGitHub {
-    owner = "dwlehman";
+    owner = "storaged-project";
     repo = "blivet";
     rev = name;
-    sha256 = "1gk94ghjrxfqnx53hph1j2s7qcv86fjz48is7l099q9c24rjv8ky";
+    sha256 = "182kkh4fnja109vx6bgwgrgg3qsajhkw2bl1kji84ni0agqll5q5";
   };
 
   postPatch = ''
-    sed -i \
-      -e 's|"multipath"|"${multipath-tools}/sbin/multipath"|' \
-      -e '/^def set_friendly_names/a \    return False' \
-      blivet/devicelibs/mpath.py
-    sed -i -e '/"wipefs"/ {
-      s|wipefs|${utillinux}/sbin/wipefs|
-      s/-f/--force/
-    }' blivet/formats/__init__.py
-    sed -i -e 's|"lsof"|"${lsof}/bin/lsof"|' blivet/formats/fs.py
-    sed -i -r -e 's|"(u?mount)"|"${utillinux}/bin/\1"|' blivet/util.py
+  #  sed -i \
+  #    -e 's|"multipath"|"${multipath-tools}/sbin/multipath"|' \
+  #    -e '/^def set_friendly_names/a \    return False' \
+  #    blivet/devicelibs/mpath.py
+  #  sed -i -e '/"wipefs"/ {
+  #    s|wipefs|${utillinux}/sbin/wipefs|
+  #    s/-f/--force/
+  #  }' blivet/formats/__init__.py
+  #  sed -i -e 's|"lsof"|"${lsof}/bin/lsof"|' blivet/formats/fs.py
+  #  sed -i -r -e 's|"(u?mount)"|"${utillinux}/bin/\1"|' blivet/util.py
   '';
 
-  propagatedBuildInputs = [
-    pykickstart pyparted pyblock pyudev selinuxWithPython cryptsetupWithPython
+  propagatedBuildInputs = with pythonPackages; [
     six
+    pyudev
+    pyparted
+    libselinux
+    pygobject3
+    dbus-python
+    enum
+# unclear, test these
+   # pykickstart
+    pyblock
+    cryptsetupWithPython
+    # parted
+    # python-blockdev
+    # libblockdev
+    # python-bytesize
+    # python-hawkey
   ];
 
-  # Tests are in nixos/tests/blivet.nix.
+  # Cannot test in sanbox, need a VM. Tests are in nixos/tests/blivet.nix.
   doCheck = false;
 
   meta = with stdenv.lib; {
-    homepage = https://fedoraproject.org/wiki/Blivet;
-    description = "Module for management of a system's storage configuration";
+    homepage = https://github.com/storaged-project/blivet;
+    description = "A python module for system storage configuration";
     license = with licenses; [ gpl2Plus lgpl21Plus ];
     platforms = platforms.linux;
   };
