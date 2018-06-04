770583145a178a9f8138af78d5908948c3a87297
Uli Baum <xeji@cat3.de>
2018-06-04 11:55:57 +0200

    libbytesize: init at 1.3

---
+++ pkgs/development/libraries/libbytesize/default.nix
@@ -0,0 +1,32 @@
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, file, python3
+, mpfr, pcre }:
+
+stdenv.mkDerivation rec {
+  name = "libbytesize-${version}";
+  version = "1.3";
+
+  src = fetchFromGitHub {
+    owner = "storaged-project";
+    repo = "libbytesize";
+    rev = version;
+    sha256 = "1ys5d8rya8x4q34gn1hr96z7797s9gdzah0y0d7g84x5x6k50p30";
+  };
+
+  nativeBuildInputs = [ autoreconfHook file pkgconfig python3 ];
+  buildInputs = [ mpfr pcre ]; 
+
+  # for python bindings
+  propagatedBuildImports = with python3.pkgs; [ six ];
+
+  preConfigure = ''
+    sed -i "s:/usr/bin/file:${file}/bin/file:g" configure
+  '';
+
+  meta = with stdenv.lib; {
+    homepage = src.meta.homepage;
+    description = "A C library for reading, creating and modifying zip archives";
+    license = licenses.lgpl21Plus;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ xeji ];
+  };
+}
